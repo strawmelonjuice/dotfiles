4b66a6ff3239a75f9ef84bb3f2d4e30c0f3f9d21
Mar <mar@strawmelonjuice.com>
2024-09-10 08:17:47 +0200

    Update Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1

--- readonly_Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,5 +1,16 @@
+# Zoxide
+
 Invoke-Expression (& { (zoxide init powershell | Out-String) })
 Remove-Item alias:cd
 new-alias cd z
 echo "Zoxide loaded."
 
+# Other aliases
+Remove-Item alias:cls
+new-alias cls clear
+
+Remove-Item alias:q
+new-alias q exit
+
+Remove-Item alias:qa
+new-alias qa exit
