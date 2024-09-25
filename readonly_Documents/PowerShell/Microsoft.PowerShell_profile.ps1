216e95be4b140950333000d06752235104b869f2
Mar <mar@strawmelonjuice.com>
2024-09-25 12:29:23 +0200

    Update Documents/PowerShell/Microsoft.PowerShell_profile.ps1 Add Documents/PowerShell/moreadvancedprofile.ps1 Remove Documents/PowerShell/simplerprofile.ps1

--- readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -10,75 +10,11 @@ if (!$PSVersionTable.PSVersion.toString().startsWith("7"))
 # Define functions
 function quit
 { exit
-}
-function make-link ($target, $link)
-{
-  New-Item -Path $link -ItemType SymbolicLink -Value $target
+}function advancedstub
+{ 
+  . ($ENV:Userprofile + "\Documents\PowerShell\moreadvancedprofile.ps1")
 }
 
-# Check if Windows
-if ($IsWindows)
-{
-  # Checking installed packages
-  write-host "Checking installed packages..."
-  $packages = @(
-    "LLVM.clangd",
-    "zig.zig",
-    "zigtools.zls",
-    "Oven-sh.Bun",
-    "Rustlang.Rustup",
-    "ajeetdsouza.zoxide",
-    "Starship.Starship",
-    "Neovim.Neovim",
-    "Fastfetch-cli.Fastfetch",
-    "twpayne.chezmoi",
-    "Mozilla.Firefox.DeveloperEdition",
-    "JetBrains.Toolbox",
-    "StartIsBack.StartAllBack",
-    "bitwarden.bitwarden",
-    "JesseDuffield.lazygit",
-    "Git.Git",
-    "HermannSchinagl.LinkShellExtension",
-    "Valve.Steam",
-    "Discord.Discord",
-    "Gleam.Gleam",
-    "Python.Python.3.12",
-    "Anaconda.Anaconda3",
-    "junegunn.fzf",
-    "GitHub.GitHubDesktop",
-    "GitHub.cli",
-    "BurntSushi.ripgrep.MSVC"
-  )
-  $packages | ForEach-Object {
-    winget list -q $_ | Out-Null
-    if ($?)
-    {
-      # Write-Host "$_ installed"
-    } else
-    {
-      Write-Host "Package $_ not found. Installing..."
-      winget install $_
-    }
-  }
-  write-host "All packages installed."
-
-  # Create symbolic links
-  Write-Host "Creating symbolic links..."
-  $symlinks = @(
-    @(($ENV:UserProfile + "\.config\nvim"), ($ENV:LocalAppdata + "\nvim")),
-    @(($ENV:UserProfile + "\.config\alacritty"), ($ENV:Appdata + "\Alacritty")),
-    @(($ENV:UserProfile + "\.config\zed"), ($ENV:Appdata + "\Zed"))
-  )
-  foreach ($link in $symlinks)
-  {
-    if (Test-Path -Path $link[1])
-    {
-    } else
-    {
-      make-link $link[0] $link[1]
-    }
-  }
-}
 
 # Starship
 Write-Host "Loading Starship..."
@@ -101,10 +37,20 @@ if (test-path alias:qa)
   Remove-Item alias:qa
 }
 new-alias qa quit
+if (test-path alias:stub)
+{
+  Remove-Item alias:stub
+}
+new-alias stub advancedstub
 
-# Base profile
-. ($ENV:Userprofile + "\Documents\PowerShell\simplerprofile.ps1")
 
+# Zoxide
+Write-Host "Loading Zoxide..."
+Invoke-Expression (& { (zoxide init powershell --cmd cd | Out-String) })
+# if (test-path alias:cd) {
+#    Remove-Item alias:cd
+# }
+# new-alias cd z
 
 Write-Host "Preparing fastfetch..."
 Write-Host ""
--- readonly_Documents/PowerShell/simplerprofile.ps1
+++
@@ -1,7 +0,0 @@
-# Zoxide
-Write-Host "Loading Zoxide..."
-Invoke-Expression (& { (zoxide init powershell --cmd cd | Out-String) })
-# if (test-path alias:cd) {
-#    Remove-Item alias:cd
-# }
-# new-alias cd z
