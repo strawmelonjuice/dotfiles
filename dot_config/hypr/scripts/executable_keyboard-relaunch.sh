2aea45d4b3a648c8b700cff0ecd48889147c3113
MLC Bloeiman <mar@strawmelonjuice.com>
2024-12-22 21:51:46 +0100

    Update .config/hypr/conf/autostart.conf Update .config/hypr/scripts/keyboard-relaunch.sh Update .config/waybar/quicklinks.jsonc

--- dot_config/hypr/scripts/executable_keyboard-relaunch.sh
+++ dot_config/hypr/scripts/executable_keyboard-relaunch.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-# Relaunch the touch keyboard
-# kill the keyboard
-killall wvkbd-mobintl
-# restart the keyboard
-wvkbd-mobintl -L 400
+# Relaunch the touch keyboard if it crashed
+
+line=$(ps aux | grep wvkbd-mobintl)
+if [ -z "$line" ]; then
+  wvkbd-mobintl -L 400
+else
+  echo $line >/dev/null
+  # Its running, so send it a signal to toggle it up
+  kill -34 $(ps -C wvkbd-mobintl)
+fi
