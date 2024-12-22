000a23d6f0725ca1322ea3a3758caed5fd8af486
MLC Bloeiman <mar@strawmelonjuice.com>
2024-12-22 23:29:56 +0100

    Update .config/hypr/conf/autostart.conf Update .config/hypr/scripts/keyboard-relaunch.sh

--- dot_config/hypr/scripts/executable_keyboard-relaunch.sh
+++ dot_config/hypr/scripts/executable_keyboard-relaunch.sh
@@ -3,7 +3,7 @@
 
 line=$(ps -C wvkbd-mobintl --no-headers)
 if [ -z "$line" ]; then
-  wvkbd-mobintl -L 400
+  wvkbd-mobintl -L 300
 else
   echo $line >/dev/null
   # Its running, so send it a signal to toggle it up
