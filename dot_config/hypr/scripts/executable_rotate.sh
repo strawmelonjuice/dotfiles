1b0076b9a0b3c8c1befcf0d111e42dba26dbf5f1
MLC Bloeiman <mar@strawmelonjuice.com>
2025-01-10 22:59:54 +0100

    Update .config/hypr/scripts/rotate.sh

--- dot_config/hypr/scripts/executable_rotate.sh
+++ dot_config/hypr/scripts/executable_rotate.sh
@@ -3,4 +3,5 @@ if [ -f ~/rotated.temp.lock ] && grep -q "1" ~/rotated.temp.lock; then
   hyprctl keyword monitor ,preferred,auto,1,transform,toggle,0
 else
   hyprctl keyword monitor ,preferred,auto,1,transform,toggle,1
+  echo "1" >~/rotated.temp.lock
 fi
