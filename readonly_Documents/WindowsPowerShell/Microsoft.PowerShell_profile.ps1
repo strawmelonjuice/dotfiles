c3a32c78c58122d90011807496a429f994271892
Mar <mar@strawmelonjuice.com>
2025-01-12 17:42:42 +0100

    Update Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1

--- readonly_Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,5 @@
-Write-Host "Powershell version" $PSVersionTable.PSVersion "was detected. Running pwsh instead."
-pwsh
-Write-Host "Exiting Powershell..."
-Stop-Process -Id $PID -Force -PassThru
+#Write-Host "Powershell version" $PSVersionTable.PSVersion "was detected. Running pwsh instead."
+#pwsh
+#Write-Host "Exiting Powershell..."
+#Stop-Process -Id $PID -Force -PassThru
+Write-Host "Powershell version" $PSVersionTable.PSVersion "was detected. For Powershell 7, run pwsh instead."
