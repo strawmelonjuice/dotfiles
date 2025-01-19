ac0fb86a02484f206ec9e3bfc4febcc1c2ab10ca
MLC Bloeiman <mar@strawmelonjuice.com>
2025-01-19 22:24:02 +0100

    Update .config/sway/conf/binds.conf Update .config/sway/scripts/autostart.sh

--- dot_config/sway/scripts/executable_autostart.sh
+++ dot_config/sway/scripts/executable_autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/env bash
 # This script is used to start the executable files on the right workspaces
 swaymsg "workspace 2; exec ~/shellscripts/browser.sh"
-sleep 2 && swaymsg "workspace 3; exec discord"
-sleep 2 && swaymsg "workspace 4; exec spotify"
+sleep 1 && swaymsg "workspace 3; exec discord"
+sleep 4 && swaymsg "workspace 4; exec spotify"
 
-sleep 5 && swaymsg "workspace 1"
+sleep 2 && swaymsg "workspace 1"
