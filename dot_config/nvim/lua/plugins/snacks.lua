f54d8a6c7e4a824cc8d689d54b663258e364badd
MLC Bloeiman <mar@strawmelonjuice.com>
2025-02-03 19:55:29 +0100

    Update .config/nvim/lua/plugins/snacks.lua

--- dot_config/nvim/lua/plugins/snacks.lua
+++ dot_config/nvim/lua/plugins/snacks.lua
@@ -27,7 +27,7 @@ return {
           { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
           { icon = " ", key = "s", desc = "Restore Session", section = "session" },
           { icon = " ", key = "x", desc = "Lazy Extras", action = ":LazyExtras" },
-          { icon = "󰒲 ", key = "l", desc = "Lazy", action = ":Lazy" },
+          { icon = "", key = "l", desc = "Plugins", action = ":Lazy" },
           { icon = " ", key = "q", desc = "Quit", action = ":qa" },
         },
       },
