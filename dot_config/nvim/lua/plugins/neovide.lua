26ee207b62afb51f3ac366116a22df7a271017a4
MLC Bloeiman <mar@strawmelonjuice.com>
2025-02-09 14:14:40 +0100

    Update .config/nvim/lua/plugins/neovide.lua

--- dot_config/nvim/lua/plugins/neovide.lua
+++ dot_config/nvim/lua/plugins/neovide.lua
@@ -5,6 +5,11 @@ if vim.g.neovide then
   vim.g.neovide_transparency = 0.85
   vim.g.neovide_cursor_vfx_mode = "pixiedust"
   vim.g.neovide_cursor_smooth_blink = true
+  vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.neovid_mouse_cursor_icon = "i-beam"
+  vim.g.neovide_cursor_vfx_particle_lifetime = 4.5
+  vim.g.neovide_cursor_vfx_particle_density = 7.0
+  vim.g.neovide_cursor_vfx_opacity = 200.0
 end
 
 return {}
