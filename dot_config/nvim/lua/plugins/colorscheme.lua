876f6dcfa970c5e0e92bded32d142c3dcb6e6b55
MLC Bloeiman <mar@strawmelonjuice.com>
2025-02-21 00:30:48 +0100

    Update .config/nvim/lua/plugins/colorscheme.lua

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -42,6 +42,30 @@ local get_colorscheme = function(a)
     end
   end
 end;
+local get_background = function(a)
+  if a == "random" then
+    math.randomseed(os.time())
+    math.random(); math.random(); math.random();
+    local random = math.ceil((math.random(0.1, 1.9) / 2)) - 1;
+    vim.notify_once("[Random background]: random = " .. random .. " - " .. (random == 1 and "light" or "dark"))
+
+    if random == 1 then return "light" end
+    if random == 0 then return "dark" end
+    return "default"
+  else
+    if a == "tod" then
+      local tod = get_time_of_day()
+      if (tod == "morning") or (tod == "afternoon") then
+        vim.notify_once("Good " .. tod .. " Mar! background auto set to: light")
+        return "light"
+      end
+      if (tod == "evening") or (tod == "night") then
+        vim.notify_once("Good " .. tod .. " Mar! background auto set to: dark")
+        return "dark"
+      end
+    end
+  end
+end;
 return {
   { "haystackandroid/carbonized" },
   { "xiyaowong/transparent.nvim" },
@@ -174,12 +198,37 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = get_colorscheme("tod")
+      colorscheme = get_colorscheme("tod"),
+      background = get_background("tod")
     },
     keys = {
-      { "<leader>Md", function() vim.cmd("colorscheme " .. default_dark) end,  mode = { "n", "t" }, desc = "Set to " .. default_dark .. " theme." },
-      { "<leader>Ml", function() vim.cmd("colorscheme " .. default_light) end, mode = { "n", "t" }, desc = "Set to " .. default_light .. " theme." },
-      { "<leader>Mg", function() vim.cmd("colorscheme " .. default_show) end,  mode = { "n", "t" }, desc = "Set to " .. default_show .. " theme." },
+      {
+        "<leader>Md",
+        function()
+          vim.cmd("colorscheme " .. default_dark)
+          vim.cmd("set background=dark")
+        end,
+        mode = { "n", "t" },
+        desc = "Set to " .. default_dark .. " theme."
+      },
+      {
+        "<leader>Ml",
+        function()
+          vim.cmd("colorscheme " .. default_light)
+          vim.cmd("set background=light")
+        end,
+        mode = { "n", "t" },
+        desc = "Set to " .. default_light .. " theme."
+      },
+      {
+        "<leader>Mg",
+        function()
+          vim.cmd("colorscheme " .. default_show)
+          vim.cmd("set background=default")
+        end,
+        mode = { "n", "t" },
+        desc = "Set to " .. default_show .. " theme."
+      },
       -- { "<leader>Mf", function() vim.cmd("TransparentToggle") end,             mode = { "n", "t" }, desc = "Toggle transparency." },
 
     },
