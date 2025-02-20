3cf93bd1beab7d608fb3d25b32390738742f9442
MLC Bloeiman <mar@strawmelonjuice.com>
2025-02-21 00:23:58 +0100

    Update .config/nvim/lua/plugins/colorscheme.lua

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,7 +1,8 @@
 vim.opt.termguicolors = true
 -- local default_light = "peachpuff"
 local default_light = "carbonized-light"
-local default_dark = "kanagawa-wave"
+local default_dark = "melange"
+-- local default_dark = "kanagawa-wave"
 -- local default_dark = "rose-pine-main"
 local default_show = "zaibatsu"
 
