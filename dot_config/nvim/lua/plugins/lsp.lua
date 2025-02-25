291725f3903aa187a22e3f6ccb1a2e55f830ce49
MLC Bloeiman <mar@strawmelonjuice.com>
2025-02-25 15:45:03 +0100

    Update .config/nvim/lua/plugins/lsp.lua

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -61,6 +61,29 @@ return {
     }
   },
   {
+    "tpope/vim-dadbod",
+    enabled = false,
+  },
+  {
+    "tpope/vim-dadbod-ui",
+    enabled = false,
+  },
+  {
+    "kndndrj/nvim-dbee",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+    },
+    build = function()
+      -- Install tries to automatically detect the install method.
+      -- if it fails, try calling it with one of these parameters:
+      --    "curl", "wget", "bitsadmin", "go"
+      require("dbee").install()
+    end,
+    config = function()
+      require("dbee").setup( --[[optional config]])
+    end,
+  },
+  {
     "williamboman/mason.nvim",
     opts = {
       ensure_installed = {
