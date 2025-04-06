437e737756019d4c8e9d35f6acc47f56e14cf587
MLC Bloeiman <mar@strawmelonjuice.com>
2025-04-06 23:38:38 +0200

    Update .config/hypr/conf/autostart.conf Update .config/sway/scripts/autostart.sh Update shellscripts/fullinstall.sh

--- dot_config/sway/scripts/executable_autostart.sh
+++ dot_config/sway/scripts/executable_autostart.sh
@@ -2,6 +2,6 @@
 # This script is used to start the executable files on the right workspaces
 swaymsg "workspace 2; exec ~/shellscripts/browser.sh"
 sleep 1 && swaymsg "workspace 3; exec discord"
-sleep 4 && swaymsg "workspace 4; exec spotify"
+sleep 4 && swaymsg "workspace 4; exec tidal-hifi"
 
 sleep 2 && swaymsg "workspace 1"
