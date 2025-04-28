fbcdbd10b71ee834a8ed2fbe6d8e08c47a9c9ab5
MLC Bloeiman <mar@strawmelonjuice.com>
2025-04-28 20:24:30 +0200

    Update .config/zshrc/00-init.sh

--- dot_config/zshrc/00-init.sh
+++ dot_config/zshrc/00-init.sh
@@ -7,16 +7,12 @@ autoload -Uz compinit && compinit
 
 # Zellij if on any of my main terminals
 # Zellij needs to start first, because otherwise we'll be going through the entire zshrc twice.
-if [ "${TERM}" != "alacritty" ] && [ "${TERM}" != "xterm-ghostty" ] && [ "${TERM}" != "contour" ] && [ "${TERM}" != "foot" ] && [ "${$(ps -p "$PPID" -o comm=)}" != "cosmic-term" ]; then
-  # no need to state this for kitty, as it is the default terminal but does not need to autostart Zellij.
-  if [ "${TERM}" != "xterm-kitty" ]; then
-    echo "Not autostarting Zellij as terminal is different from usual."
-  fi
-else
-  echo "Starting zellij"
+if [ "${TERM}" = "alacritty" ] || [ "${TERM}"  = "xterm-ghostty" ] || [ "${TERM}"  = "contour" ] || [ "${TERM}"  = "foot" ] || [ "${$(ps -p "$PPID" -o comm=)}" =  "cosmic-term" ]; then
   # export ZELLIJ_AUTO_ATTACH=true
   export ZELLIJ_AUTO_EXIT=true
   eval "$(zellij setup --generate-auto-start zsh)"
+elseif [ "${TERM}"  = "xterm-kitty" ]
+  echo "Not starting zellij as terminal is not recognised"
 fi
 
 # -----------------------------------------------------
