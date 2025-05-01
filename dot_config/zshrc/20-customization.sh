27f6e0684d5c8e0238943ba59846590ee0423eda
MLC Bloeiman <mar@strawmelonjuice.com>
2025-05-01 22:24:45 +0200

    Update .config/zellij/layouts/ide.kdl Update .config/zshrc/10-aliases.sh Update .config/zshrc/20-customization.sh

--- dot_config/zshrc/20-customization.sh
+++ dot_config/zshrc/20-customization.sh
@@ -3,30 +3,6 @@
 # -----------------------------------------------------
 
 # -----------------------------------------------------
-# oh-myzsh themes: https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
-# -----------------------------------------------------
-ZSH_THEME=fino-time
-
-# -----------------------------------------------------
-# oh-myzsh plugins
-# -----------------------------------------------------
-plugins=(
-  git
-  sudo
-  web-search
-  archlinux
-  zsh-autosuggestions
-  copyfile
-  copybuffer
-  dirhistory
-)
-
-# # If zsh-syntax-highlighting is not installed, install it with `yay -S zsh-syntax-highlighting`
-
-# Set-up oh-my-zsh
-source $ZSH/oh-my-zsh.sh
-
-# -----------------------------------------------------
 # Set-up FZF key bindings (CTRL R for fuzzy history finder)
 # -----------------------------------------------------
 source <(fzf --zsh)
