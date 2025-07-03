e6c4cfd68132d4a9a6268dc6d1819ea85063f0c6
MLC Bloeiman <mar@strawmelonjuice.com>
2025-07-03 18:59:43 +0200

    no greet

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,7 +1,7 @@
 # -----------------------------------------------------
 # Fish Configuration
 # -----------------------------------------------------
-
+set fish_greeting ""
 # Load modular Fish configuration files
 for file in ~/.config/fish/config/*.fish
     if test -f $file
