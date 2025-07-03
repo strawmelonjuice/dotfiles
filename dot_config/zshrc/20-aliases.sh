673b1268a3b13d41f01f9425860a26c8b139d29d
MLC Bloeiman <mar@strawmelonjuice.com>
2025-07-03 15:51:42 +0200

    Remove vimlike aliases

--- dot_config/zshrc/20-aliases.sh
+++ dot_config/zshrc/20-aliases.sh
@@ -24,11 +24,6 @@ alias yay='paru'
 
 alias update-grub='sudo grub-mkconfig -o /boot/grub/grub.cfg'
 
-# I get Vim muscle memory in the terminal...
-alias qa='echo "Yes, you can use :qa to exit the entire session."'
-alias q='echo "Yes, you can use :q to exit the current tab."'
-alias :qa='zellij kill-session $ZELLIJ_SESSION_NAME'
-alias :q='zellij action close-tab'
 # I'm lazy and don't want to type the full command
 alias ide='zellij --layout ide'
 alias strider='zellij plugin --in-place -- zellij:strider'
