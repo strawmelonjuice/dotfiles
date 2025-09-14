8bce9510f6b8405fc128000b7beb032bac65015b
MLC Bloeiman <mar@strawmelonjuice.com>
2025-09-14 17:57:24 +0200

    Cleaning this up (I stole @nnuuvv 's approach)

--- dot_config/exact_nvim/lua/plugins/lsp.lua
+++ dot_config/exact_nvim/lua/plugins/lsp.lua
@@ -1,327 +1,143 @@
-local lspconfig = require "mason-lspconfig"
 return {
-  {
-    'hrsh7th/nvim-cmp',
-    enabled = false
+  "neovim/nvim-lspconfig",
+  dependencies = {
+    { "williamboman/mason.nvim", lazy = true },
+    "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    { "j-hui/fidget.nvim",       opts = {} },
+    "simrat39/rust-tools.nvim",
+    { 'nvim-telescope/telescope.nvim', tag = '0.1.8', },
+    -- "hrsh7th/cmp-nvim-lsp", -- Verwijderd omdat je blink.cmp gebruikt
   },
-  {
-    'saghen/blink.cmp',
-    -- optional: provides snippets for the snippet source
-    dependencies = 'rafamadriz/friendly-snippets',
+  config = function()
+    local lspconfig = require('lspconfig')
+    local capabilities = vim.lsp.protocol.make_client_capabilities()
+    -- capabilities = vim.tbl_deep_extend('force', {}, capabilities, require('cmp_nvim_lsp').default_capabilities()) -- Deze regel is niet meer nodig
 
-    -- use a release tag to download pre-built binaries
-    version = '*',
-    -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-    -- build = 'cargo build --release',
-    -- If you use nix, you can build from source using latest nightly rust with:
-    -- build = 'nix run .#build-plugin',
-
-    ---@module 'blink.cmp'
-    ---@type blink.cmp.Config
-    opts = {
-      -- 'default' for mappings similar to built-in completion
-      -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
-      -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
-      -- See the full "keymap" documentation for information on defining your own keymap.
-      keymap = { preset = 'enter' },
-
-      appearance = {
-        -- Sets the fallback highlight groups to nvim-cmp's highlight groups
-        -- Useful for when your theme doesn't support blink.cmp
-        -- Will be removed in a future release
-        use_nvim_cmp_as_default = true,
-        -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-        -- Adjusts spacing to ensure icons are aligned
-        nerd_font_variant = 'mono'
-      },
-
-      -- Default list of enabled providers defined so that you can extend it
-      -- elsewhere in your config, without redefining it, due to `opts_extend`
-      sources = {
-        default = { 'lsp', 'path', 'snippets', 'buffer' },
-      },
-    },
-    opts_extend = { "sources.default" }
-  },
-  {
-    "fladson/vim-kitty",
-    ft = "kitty",
-  }
-  ,
-  --  { import = "lazyvim.plugins.extras.lang.json" },
-  { import = "lazyvim.plugins.extras.lang.typescript" },
-  {
-    'nvimdev/lspsaga.nvim',
-    config = function()
-      require('lspsaga').setup({})
-    end,
-    dependencies = {
-      'nvim-treesitter/nvim-treesitter', -- optional
-      'nvim-tree/nvim-web-devicons',     -- optional
-    }
-  },
-  {
-    "tpope/vim-dadbod",
-    enabled = false,
-  },
-  {
-    "tpope/vim-dadbod-ui",
-    enabled = false,
-  },
-  {
-    "kndndrj/nvim-dbee",
-    dependencies = {
-      "MunifTanjim/nui.nvim",
-    },
-    build = function()
-      -- Install tries to automatically detect the install method.
-      -- if it fails, try calling it with one of these parameters:
-      --    "curl", "wget", "bitsadmin", "go"
-      require("dbee").install()
-    end,
-    config = function()
-      require("dbee").setup( --[[optional config]])
-    end,
-  },
-  {
-    "williamboman/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "biome",
-        "clangd",
-        -- I also have rustaceanvim
-        -- "rust-analyzer",
-        -- "rustfmt", No longer installed through mason
-        "typescript-language-server",
-        "stylua",
-        "tailwindcss-language-server",
-        "codelldb",
-        "css-lsp",
-        "html-lsp",
-        "zls",
-        "rustywind",
-        "impl",
-        "gopls",
-        "taplo",
-        "dhall-lsp",
-        "woke",
-        "intelephense",
-        "crystalline"
-      },
-    },
-  },
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "javascript",
-        -- "json", afraid this would hit me with the microsoft json language server, which is shit.
-        -- See line 12
-        -- "rust",
-        "typescript",
-        "yaml",
-        "gleam"
-      })
-    end,
-  },
-  {
-    "stevearc/conform.nvim",
-    dependencies = { "mason.nvim" },
-    lazy = true,
-    cmd = "ConformInfo",
-    keys = {
-      {
-        "<leader>cF",
-        function()
-          require("conform").format({ formatters = { "injected" }, timeout_ms = 3000 })
-        end,
-        mode = { "n", "v" },
-        desc = "Format Injected Langs",
-      },
-    },
-    init = function()
-      -- Install the conform formatter on VeryLazy
-      LazyVim.on_very_lazy(function()
-        LazyVim.format.register({
-          name = "conform.nvim",
-          priority = 100,
-          primary = true,
-          format = function(buf)
-            local opts = LazyVim.opts("conform.nvim")
-            require("conform").format(LazyVim.merge({}, opts.format, { bufnr = buf }))
-          end,
-          sources = function(buf)
-            local ret = require("conform").list_formatters(buf)
-            return vim.tbl_map(function(v)
-              return v.name
-            end, ret)
-          end,
-        })
-      end)
-    end,
-    opts = {
-      -- LazyVim will use these options when formatting with the conform.nvim formatter
-      default_format_opts = {
-        timeout_ms = 3000,
-        async = false,       -- not recommended to change
-        quiet = false,       -- not recommended to change
-        lsp_fallback = true, -- not recommended to change
-      },
-      formatters_by_ft = {
-        lua = { "stylua" },
-        fish = { "fish_indent" },
-        sh = { "shfmt" },
-        javascript = { "biome", "prettier" },
-        typescript = { "biome", "prettier" },
-        vue = { "prettier" },
-        css = { "prettier" },
-        scss = { "prettier" },
-        less = { "prettier" },
-        pcss = { "prettier" },
-        html = { "prettier" },
-        json = { "biome" },
-        jsonc = { "biome" },
-        yaml = { "prettier" },
-        markdown = { "prettier" },
-        graphql = { "prettier" },
-        handlebars = { "prettier" },
-        -- rust = { "rustfmt", "rust-analyzer" }
-        rust = { "rustfmt" },
-
-      },
-      -- The options you set here will be merged with the builtin formatters.
-      -- You can also define any custom formatters here.
-      ---@type table<string, conform.FormatterConfigOverride|fun(bufnr: integer): nil|conform.FormatterConfigOverride>
-      formatters = {
-        injected = { options = { ignore_errors = true } },
-        -- # Example of using dprint only when a dprint.json file is present
-        -- dprint = {
-        --   condition = function(ctx)
-        --     return vim.fs.find({ "dprint.json" }, { path = ctx.filename, upward = true })[2]
-        --   end,
-        -- },
-        --
-        -- # Example of using shfmt with extra args
-        -- shfmt = {
-        --   prepend_args = { "-i", "2", "-ci" },
-        -- },
-      },
-      init = function()
-        -- If you want the formatexpr, here is the place to set it
-        vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
-        vim.g.rustaceanvim = {
-          server = {
-            cmd = function()
-              local mason_registry = require('mason-registry')
-              local ra_binary = mason_registry.is_installed('rust-analyzer')
-                  -- This may need to be tweaked, depending on the operating system.
-                  and mason_registry.get_package('rust-analyzer'):get_install_path() .. "/rust-analyzer"
-                  or "rust-analyzer"
-              return { ra_binary } -- You can add args to the list, such as '--log-file'
-            end,
+    local servers = {
+      lua_ls = {
+        settings = {
+          Lua = {
+            completion = {
+              callSnipped = 'Replace',
+            },
           },
-        }
-
-        lspconfig.gleam.setup({})
-      end,
+        },
+      },
+      rust_analyzer = {
+        settings = {
+          ['rust_analyzer'] = {
+            cargo = {
+              allFeatures = true,
+            },
+          },
+        },
+      },
     }
-  },
-  { "hiphish/rainbow-delimiters.nvim" },
-  "mfussenegger/nvim-lint",
-  event = "LazyFile",
-  opts = {
-    -- Event to trigger linters
-    events = { "BufWritePost", "BufReadPost", "InsertLeave" },
-    linters_by_ft = {
-      fish = { "fish" },
-      postcss = { "prettier" },
-      css = { "prettier" },
-      -- Use the "*" filetype to run linters on all filetypes.
-      -- ['*'] = { 'global linter' },
-      -- Use the "_" filetype to run linters on filetypes that don't have other linters configured.
-      -- ['_'] = { 'fallback linter' },
-      -- ["*"] = { "typos" },
-    },
-    -- LazyVim extension to easily override linter options
-    -- or add custom linters.
-    ---@type table<string,table>
-    linters = {
-      -- -- Example of using selene only when a selene.toml file is present
-      -- selene = {
-      --   -- `condition` is another LazyVim extension that allows you to
-      --   -- dynamically enable/disable linters based on the context.
-      --   condition = function(ctx)
-      --     return vim.fs.find({ "selene.toml" }, { path = ctx.filename, upward = true })[1]
-      --   end,
-      -- },
-    },
-  },
-  config = function(_, opts)
-    local M = {}
 
-    local lint = require("lint")
-    for name, linter in pairs(opts.linters) do
-      if type(linter) == "table" and type(lint.linters[name]) == "table" then
-        lint.linters[name] = vim.tbl_deep_extend("force", lint.linters[name], linter)
-        if type(linter.prepend_args) == "table" then
-          lint.linters[name].args = lint.linters[name].args or {}
-          vim.list_extend(lint.linters[name].args, linter.prepend_args)
+    require('mason').setup()
+
+    local ensure_installed = vim.tbl_keys(servers or {})
+    vim.list_extend(ensure_installed, { 'stylua' })
+    require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+
+    require('mason-lspconfig').setup {
+      ensure_installed = {},
+      automatic_installation = {},
+      handlers = {
+        function(server_name)
+          local server = servers[server_name] or {}
+          server.capabilities = vim.tbl_deep_extend('force', {}, capabilities, server.capabilities or {})
+          require('lspconfig')[server_name].setup(server)
+        end,
+      },
+    }
+
+    lspconfig.gleam.setup({
+      capabilities = capabilities,
+    })
+
+    local is_windows = vim.fn.has("win64") == 1 or vim.fn.has("win32") == 1 or vim.fn.has("win16") == 1
+    local omnisharp_path = ""
+    if is_windows then
+      omnisharp_path = vim.fs.normalize("~/omnisharp/OmniSharp.exe")
+    else
+      omnisharp_path = vim.fs.normalize("~/omnisharp/OmniSharp")
+    end
+
+    lspconfig.omnisharp.setup({
+      cmd = { omnisharp_path, '--languageserver' },
+      filetypes = { 'cs', 'razor' },
+      root_dir = lspconfig.util.root_pattern('*.sln', 'Directory.Build.props', '.git'),
+      capabilities = capabilities,
+      settings = {
+        FormattingOptions = {
+          EnableEditorConfigSupport = true,
+          OrganizeImports = true,
+        },
+        MsBuild = {
+          LoadProjectsOnDemand = nil,
+        },
+        RoslynExtensionsOptions = {
+          EnableImportCompletion = true,
+          AnalyzeOpenDocumentsOnly = nil,
+        },
+        Sdk = {
+          IncludePrereleases = true,
+        },
+      },
+    })
+
+    vim.api.nvim_create_autocmd('LspAttach', {
+      group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+      callback = function(event)
+        local map = function(keys, func, desc, mode)
+          mode = mode or 'n'
+          vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
         end
-      else
-        lint.linters[name] = linter
-      end
-    end
-    lint.linters_by_ft = opts.linters_by_ft
 
-    function M.debounce(ms, fn)
-      local timer = vim.uv.new_timer()
-      return function(...)
-        local argv = { ... }
-        timer:start(ms, 0, function()
-          timer:stop()
-          vim.schedule_wrap(fn)(unpack(argv))
-        end)
-      end
-    end
+        map('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+        map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+        map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+        map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+        map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+        map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+        map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+        map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+        map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclartion')
 
-    function M.lint()
-      -- Use nvim-lint's logic first:
-      -- * checks if linters exist for the full filetype first
-      -- * otherwise will split filetype by "." and add all those linters
-      -- * this differs from conform.nvim which only uses the first filetype that has a formatter
-      local names = lint._resolve_linter_by_ft(vim.bo.filetype)
+        local client = vim.lsp.get_client_by_id(event.data.client_id)
+        if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+          local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = true })
+          vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+            buffer = event.buf,
+            group = highlight_augroup,
+            callback = vim.lsp.buf.document_highlight,
+          })
 
-      -- Create a copy of the names table to avoid modifying the original.
-      names = vim.list_extend({}, names)
+          vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+            buffer = event.buf,
+            group = highlight_augroup,
+            callback = vim.lsp.buf.clear_references,
+          })
 
-      -- Add fallback linters.
-      if #names == 0 then
-        vim.list_extend(names, lint.linters_by_ft["_"] or {})
-      end
-
-      -- Add global linters.
-      vim.list_extend(names, lint.linters_by_ft["*"] or {})
-
-      -- Filter out linters that don't exist or don't match the condition.
-      local ctx = { filename = vim.api.nvim_buf_get_name(0) }
-      ctx.dirname = vim.fn.fnamemodify(ctx.filename, ":h")
-      names = vim.tbl_filter(function(name)
-        local linter = lint.linters[name]
-        if not linter then
-          LazyVim.warn("Linter not found: " .. name, { title = "nvim-lint" })
+          vim.api.nvim_create_autocmd({ 'LspDetach' }, {
+            group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+            callback = function(event2)
+              vim.lsp.buf.clear_references()
+              vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+            end,
+          })
         end
-        return linter and not (type(linter) == "table" and linter.condition and not linter.condition(ctx))
-      end, names)
+        if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint) then
+          vim.lsp.inlay_hint.enable(true, { bufnr = event.buf })
 
-      -- Run linters.
-      if #names > 0 then
-        lint.try_lint(names)
-      end
-    end
-
-    vim.api.nvim_create_autocmd(opts.events, {
-      group = vim.api.nvim_create_augroup("nvim-lint", { clear = true }),
-      callback = M.debounce(100, M.lint),
+          map('<leader>th', function()
+            vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+          end, '[T]oggle Inlay [H]ints')
+        end
+      end,
     })
   end,
 }
+
