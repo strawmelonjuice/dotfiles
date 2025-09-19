530fe7b99c61e29ba34daacb6cb9631b0176a00e
MLC Bloeiman <mar@strawmelonjuice.com>
2025-09-19 12:06:43 +0200

    Update .config/nvim/lua/config/autocmds.lua

--- dot_config/exact_nvim/lua/config/autocmds.lua
+++ dot_config/exact_nvim/lua/config/autocmds.lua
@@ -3,6 +3,28 @@
 -- Add any additional autocmds here
 vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.format()]]
 
+
+  local function open_folder(data)
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if directory then
+    -- create a new, empty buffer
+    vim.cmd.enew()
+
+    -- wipe the directory buffer
+    vim.cmd.bw(data.buf)
+
+    -- change to the directory
+    vim.cmd.cd(data.file)
+
+    -- open the picker
+    Snacks.picker.files({ cwd = true })
+  end
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_folder })
+
 -- For now I see no reason to update the plugins everytime I open neovim -- UPDATE: I do now, constantly having notifs abt outdated stuff is annoying
 vim.api.nvim_create_autocmd("VimEnter", {
   callback = function()
